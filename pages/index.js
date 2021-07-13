a49dbde9211db24e932867ee15d7e5abe66bf1fb
TJ Pu <tsungjen.pu@gmail.com>
2021-07-13 14:39:19 +0200

    added static site generation on the homepage and loaded data & paths for dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
-import { getFeaturedEvents } from '../dummy-data';
+// import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/EventList';
 
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  const featuredEvents = getFeaturedEvents();
-
+export default function Home(props) {
   return (
     <div className={styles.container}>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents
+    }
+  }
 }
