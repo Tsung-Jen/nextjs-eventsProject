fc9c577159a8362076144bc902f2b10e9279ecf3
TJ Pu <tsungjen.pu@gmail.com>
2021-07-13 08:05:30 +0200

    started events project and finished allEvents, eventDetail, specificEvent pages and filteredEvent functionality

---
+++ components/ui/Button.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+import classes from "./button.module.css";
+
+function Button(props) {
+  if (props.link) { // check whether the Button has a property link
+    return (
+      <Link href={props.link}>
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  }
+
+  return (
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
